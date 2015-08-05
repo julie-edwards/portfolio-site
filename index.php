8d7af2b626fc69a40169f63819098850122df079
Julie Edwards <julie@julie-edwards.com>
2015-08-04 22:27:13 -0700

    initial commit

---
+++ index.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html id="top">
+<head>
+<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+<link rel="icon" href="/favicon.ico" type="image/x-icon">
+<meta name="robots" content="noindex, nofollow">
+<meta charset="utf-8">
+
+<title>Julie Edwards - Web Developer</title>
+	
+<link rel="stylesheet" href="css/style.css">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	
+</head>
+<body>
+<script>
+	//TODO GOOGLE ANALYTICS
+</script>
+	<header>
+		<h1><a href="/"><b>Julie</b>Edwards</a></h1>
+		<ul class="social-sprites">
+			<li id="linkedin-sprite"><a href="http://www.linkedin.com/pub/julie-edwards/55/4a7/43a" target="_blank"></a></li>
+			<li id="twitter-sprite"><a href="https://twitter.com/_JulieEdwards_" target="_blank"></a></li>
+		</ul>
+	</header>
+	<div id="nav-bar">Menu</div>
+	<div id="page">
+
+<div id="content">
+<h2>Portfolio</h2>
+<ul class="download-links">
+	<li><a target="_blank" href="docs/JulieEdwards-Resume.pdf">Download .pdf</a></li>
+	<li><a target="_blank" href="<docs/JulieEdwards-Resume.txt">Download .txt</a></li>
+</ul>	
+<section>
+	<div class="section-title">
+		<h3>Skills</h3>
+	</div><!-- section title -->
+	<div class="section-content">
+			<p><span class="skill">Languages</span> - HTML5, CSS3, JavaScript, jQuery, PHP, SQL</p>
+			<p><span class="skill">Tools</span> - Git/GitHub, Adobe Photoshop, Adobe Illustrator</p>
+			<p><span class="skill">Expertise</span> - Cross-browser and cross-platform compatability, responsive, SEO, Chrome extensions, Wordpress
+</p>
+	</div><!-- section content -->
+</section>
+<section>
+	<div class="section-title">
+		<h3>Experience</h3>
+	</div><!-- section title -->
+	<div class="section-content">
+	    <h4>Nordstrom Direct, Interactive Web Developer (contract, The Creative Group)<span class="dates">11/13 - 1/14</span></h4>
+        <p>HTML5, CSS3, JavaScript, jQuery, Chrome Extensions, Git, Photoshop</p>
+        <ul>
+            <li>Created and published new customer facing content and updated existing pages with HTML, CSS, and JS</li>
+            <li>Planned and developed a Chrome extension internal tool to help manage and assign work orders</li>
+        </ul>
+		<h4>Sound Soups, Front End Web Developer<span class="dates">6/13 - 9/13</span></h4>
+		<p>HTML5, CSS3, PHP, jQuery, SEO, Responsive Design, Illustrator, Photoshop</p>
+		<ul>
+			<li>Designed and created fully responsive and mobile friendly website</li>
+			<li>Wrote a jQuery toggle panel plugin and other functions to organize and display site information</li>
+		</ul>
+		<h4>Dailey Guitars, Front End Web Developer<span class="dates">9/12 - 6/13</span></h4>
+		<p>WordPress, HTML5, CSS3, PHP, jQuery, SEO, Illustrator, Photoshop</p>
+        <ul>
+            <li>Developed a fully hand-coded custom WordPress theme using PHP, HTML5, and JavaScript</li>
+            <li>Designed and produced original logo and branding</li>
+        </ul>
+		<h4>Amazon Partnerships, Board Member and Technology Specialist<span class="dates">4/12 - 4/13</span></h4>
+		<p>WordPress, HTML5, CSS3, PHP, JavaScript, Photoshop, Social Media</p>
+		<ul>
+			<li>Developed a new WordPress theme and coded dynamic site features using PHP and JavaScript</li>
+			<li>Created new HTML5/CSS3 page templates and updated site content</li>
+			<li>Managed social media presence including Facebook and Twitter</li>
+		</ul>
+		<h4>Volunteer Coordinator, The Nature Conservancy in Washington<span class="dates">9/11 - 7/12</span></h4>
+	</div><!-- section content -->
+</section>
+<section>
+	<div class="section-title">
+		<h3>Education</h3>
+	</div><!-- section title -->
+	<div class="section-content">
+		<h4>Seattle Central Community College<span class="dates">9/12 - current</span></h4>
+		<p>Coursework in web design and development including HTML5, CSS3, JavaScript, object-oriented programing, C#, database integration, PHP, SQL, WordPress, typography, UI/UX design, and Adobe software</p>
+		<h4>University of Washington, Seattle<span class="dates">Graduated 6/08</span></h4>
+		<p>Bachelor of Science, Aquatic and Fishery Sciences</p>
+	</div><!-- section content -->
+</section>
+</div><!-- content -->
+</div><!-- page -->
+<div id="footer-bar"></div>
+<footer>
+	<p id="copyright">&copy; <?php echo date("Y"); ?> Julie Edwards</p>
+</footer>	
+	
+	
+	<script src="js/rainbow-custom.min.js"></script>
+	<script src="js/togglator.js"></script>
+	
+	<script type="text/javascript">
+	    $(document).ready(function(){
+			$('.togglator-code').togglate();
+		})
+	</script>  
+
+
+</body>
+</html>
